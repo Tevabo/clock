0b84ce7a929ab02250beacf1585b17fef7968fc4
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-01-21 12:28:47 +0100

    added zero when necessary

--- main.js
+++ main.js
@@ -7,9 +7,19 @@ function showTime() {
     document.getElementById("hour").innerText = hour;
     document.getElementById("minute").innerText = minute;
     document.getElementById("second").innerText = second;
-    
+
     setTimeout(showTime, 1000);
+    // setTimeout(minute, 60000)
 
 }
 
 showTime()
+
+function addZero() {
+    if (second.innerText.length === 1) {
+        second.innerText = '0' + second.innerText;
+    }
+    setTimeout(addZero, 1000)
+}
+
+addZero()
